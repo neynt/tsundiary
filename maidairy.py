fa8428ced0d3c87dda613985f9252de3e5e072d5
Jim Zhang <Hyriodula@gmail.com>
2013-04-09 20:26:04 -0400

    upd maidairy

--- maidairy.py
+++ maidairy.py
@@ -3,14 +3,20 @@ import psycopg2
 import psycopg2.extras
 from flask import Flask, render_template
 
+# Set up database
+conn_string = "host='ec2-107-22-182-174.compute-1.amazonaws.com' dbname='d7lda6chqa8526' user='gnfcfjnsbihezp' password='7jvgKFTp6p09KuiQdf0-rDHnT8'"
+conn = psycopg2.connect(conn_string)
+
 # Set up Flask app
 app = Flask(__name__)
 desu = 0
 
-# Set up database
-conn_string = "host='ec2-107-22-182-174.compute-1.amazonaws.com' dbname='d7lda6chqa8526' user='gnfcfjnsbihezp' password='7jvgKFTp6p09KuiQdf0-rDHnT8'"
-conn = psycopg2.connect(conn_string)
-cursor = conn.cursor('cursor_unique_name', cursor_factory=psycopg2.extras.DictCursor)
+def init_db():
+    cursor = conn.cursor('cursor_unique_name', cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute('DROP TABLE IF EXISTS posts')
+    cursor.execute('CREATE TABLE posts ( postid varchar(80), content varchar(500) )')
+    cursor.execute('DROP TABLE IF EXISTS users')
+    cursor.execute('CREATE TABLE users ( postid varchar(80), content varchar(500) )')
 
 @app.route('/')
 def index():
