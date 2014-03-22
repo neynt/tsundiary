bff572d8181e986e633136c20d0ddd98c446a3ef
Jim Zhang <Hyriodula@gmail.com>
2014-03-22 16:45:50 -0400

    reenable tsunmessages. fix breakage if zero entries

--- tsundiary.py
+++ tsundiary.py
@@ -23,27 +23,26 @@ from collections import defaultdict
 # The ideal tsundere greeting is cold and hostile while betraying a
 # lingering sense of affection. Constantly search for it.
 prompts = [
-# The BELOVED cold shoulder
 "... well, %s?",
 "... hello, %s?",
 ## Normal "'sup" prompts
 "... did you manage to accomplish anything today, %s?",
-#"... so, %s, what did you manage to accomplish today, if anything?",
-#"Hey %s - found any new ways to make a fool of yourself?",
+"... so, %s, what did you manage to accomplish today, if anything?",
+"Hey %s - found any new ways to make a fool of yourself?",
 "How was your day, %s? Not that I care or anything...",
-#"How have you been wasting your time lately, %s?",
-#"What kind of stupid stuff were you up to today, %s?",
+"How have you been wasting your time lately, %s?",
+"What kind of stupid stuff were you up to today, %s?",
 "What kind of trouble did you get in today, %s?",
-#"What did you do today, %s? As if that would impress me...",
-#"It's your privilege that I'm wasting my time listening to you, %s...",
+"What did you do today, %s? As if that would impress me...",
+"It's your privilege that I'm wasting my time listening to you, %s...",
 "Don't get me wrong, %s, it's not like I'm worried about you.",
-#"If you think I'm gonna miss you, think again, %s.",
+"If you think I'm gonna miss you, think again, %s.",
 "I-it's not like I'm listening to you because I like you or anything, %s...", ## THIS RIGHT HERE
 ## More specific prompts
-#"How did it go, %s? ... not that I'm expecting much!",
-#"I'll forgive you, but just this time, got it, %s?",
-#"... who do you think you are, %s?",
-#"Why would you do that, %s? You are so foolish...",
+"How did it go, %s? ... not that I'm expecting much!",
+"I'll forgive you, but just this time, got it, %s?",
+"... who do you think you are, %s?",
+"Why would you do that, %s? You are so foolish...",
 ## Calling the end-user an idiot
 ##"バカバカバカ！",
 #"AAAAAH, %s, you idiot-idiot-idiot!",
@@ -52,33 +51,33 @@ prompts = [
 #"%s, you're such a fool.",
 #"%s wa baka desu.",
 #"%s no baka baka baka!",
-#"Can you be any more clueless, %s?",
-#"It's cute how you have no idea what's going on around you, %s.",
+"Can you be any more clueless, %s?",
+"It's cute how you have no idea what's going on around you, %s.",
 ## Backhanded compliments
 #"I guess you're smarter than you look, %s...",
 #"You look so good today, %s! I didn't recognize you."
 # make me happy
-#"You look good today, %s!",
-#"You may not know this, but you have many admirers, %s!",
-#"You have a really nice smile, %s! Please smile more...",
+"You look good today, %s!",
+"You may not know this, but you have many admirers, %s!",
+"You have a really nice smile, %s! Please smile more...",
 "Hi, %s!",
-#"Tell me about your day, %s!",
+"Tell me about your day, %s!",
 "What made you happy today, %s?",
 "What made you laugh today, %s?",
-#"What did you enjoy the most today, %s?",
+"What did you enjoy the most today, %s?",
 "How are you, %s?",
 "How did your day go, %s?",
-#"Feel free to let me know if something is bothering you, %s...",
+"Feel free to let me know if something is bothering you, %s...",
 #"I admire your optimism, %s!",
 "I love listening to you, %s!",
 #"I'm so lucky to have you, %s!",
-#"I'm so happy I'm your friend, %s!",
+"I'm so happy I'm your friend, %s!",
 #"I hope you can find happiness, %s!",
 #"I really like your personality, %s!",
 #"Keep on going, %s!",
 #"%s, m-may I have a hug?",
 #"%s, w-would you like a hug?",
-u"%sのことが大好きです！(*/////∇/////*)",
+#u"%sのことが大好きです！(*/////∇/////*)",
 ]
 
 # Set up Flask app
