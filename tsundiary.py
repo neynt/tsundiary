84aec33ac90fa1a7db00780b47a473a0ec75c42b
Jim Zhang <Hyriodula@gmail.com>
2014-04-07 19:33:17 -0400

    add userlist back

--- tsundiary.py
+++ tsundiary.py
@@ -478,34 +478,33 @@ def register_action():
     return redirect('/register')
 
 # List of users.
-@app.route('/userlist<the_rest>')
-def userlist(the_rest):
-    #all_users = (User.query.order_by(User.num_entries.desc())
-    #        .filter(User.publicity >= 2)
-    #        .filter(User.num_entries >= 2)
-    #        .all())
-    #return render_template('userlist.html', all_users=all_users)
-    return page_not_found()
+@app.route('/userlist')
+def userlist():
+    all_users = (User.query.order_by(User.num_entries.desc())
+            .filter(User.publicity >= 2)
+            .filter(User.num_entries >= 2)
+            .all())
+    return render_template('userlist.html', all_users=all_users)
 
-## List of users (sorted by latest new post)
-#@app.route('/userlist/latest')
-#def userlist_latest():
-#    all_users = (User.query.order_by(User.latest_post_date.desc())
-#            .order_by(User.num_entries.desc())
-#            .filter(User.publicity >= 2)
-#            .filter(User.num_entries >= 2)
-#            .all())
-#    return render_template('userlist.html', all_users=all_users)
-#
-## List of users (including throwaways).
-#@app.route('/userlist/all')
-#def userlist_all():
-#    all_users = (User.query.order_by(User.latest_post_date.desc())
-#            .order_by(User.num_entries.desc())
-#            .filter(User.publicity >= 2)
-#            .filter(User.num_entries >= 1)
-#            .all())
-#    return render_template('userlist.html', all_users=all_users)
+# List of users (sorted by latest new post)
+@app.route('/userlist/latest')
+def userlist_latest():
+    all_users = (User.query.order_by(User.latest_post_date.desc())
+            .order_by(User.num_entries.desc())
+            .filter(User.publicity >= 2)
+            .filter(User.num_entries >= 2)
+            .all())
+    return render_template('userlist.html', all_users=all_users)
+
+# List of users (including throwaways).
+@app.route('/userlist/all')
+def userlist_all():
+    all_users = (User.query.order_by(User.latest_post_date.desc())
+            .order_by(User.num_entries.desc())
+            .filter(User.publicity >= 2)
+            .filter(User.num_entries >= 1)
+            .all())
+    return render_template('userlist.html', all_users=all_users)
 
 @app.route('/h-hello...')
 @app.route('/about')
