d0cf3a62a47e7a03eff83417178dff333f80e8b6
Jim Zhang <hyriodula@gmail.com>
2014-10-14 00:34:36 -0400

    significantly improve colorful theme

--- tsundiary/views/theme_color.py
+++ tsundiary/views/theme_color.py
@@ -8,13 +8,6 @@ class Color():
         self.s = s/100.0
         self.v = v/100.0
 
-    def mapValue(self, min_val, max_val):
-        self.v = min_val + self.v * (max_val-min_val)
-
-    def scaleSaturation(self, max_sat):
-        """ Linearly maps the existing saturation (0.0, 1.0) to a new range (0, max_sat). """
-        self.s = self.s * max_sat
-
     def getRGB(self):
         print("cuz we're a", self.h, self.s, self.v)
         return colorsys.hsv_to_rgb(self.h,self.s,self.v)
@@ -26,6 +19,21 @@ class Color():
         g = min(g*256, 255)
         b = min(b*256, 255)
         return "#%02x%02x%02x" % (r,g,b)
+
+    def getBrightness(self):
+        """ Calculates the perceived brightness of a color (0.0, 1.0) """
+        r,g,b = self.getRGB()
+        return (0.241*r*r + 0.691*g*g + 0.068*b*b)**0.5
+
+    def scaleBrightness(self, max_val):
+        self.v *= max_val
+
+    def mapValue(self, min_val, max_val):
+        self.v = min_val + self.v * (max_val-min_val)
+
+    def scaleSaturation(self, max_sat):
+        """ Linearly maps the existing saturation (0.0, 1.0) to a new range (0, max_sat). """
+        self.s = self.s * max_sat
 
 # Theme colors.
 @app.route('/colors/<theme_name>/<colors>.css', methods=['GET'])
@@ -40,6 +48,8 @@ def theme_color(theme_name, colors):
         bg.mapValue(0.5, 1.0)
         bg.scaleSaturation(0.1)
         btn = Color(h, s, v)
+        if btn.getBrightness() > 0.6:
+             btn.scaleBrightness(0.8)
         colors.append("body { background-color: %s; }" % bg.getHexRGB())
         colors.append("a { color: %s; }" % btn.getHexRGB())
         colors.append("div#nav a { color: %s; }" % btn.getHexRGB())
