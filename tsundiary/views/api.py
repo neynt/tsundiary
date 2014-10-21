89dcf40200d2bfdc631ba2ea71fd1ea5af4bef71
Jim Zhang <hyriodula@gmail.com>
2014-10-21 00:43:37 -0400

    fix bug where loading without timezone cookie near midnight would bork things

--- tsundiary/views/api.py
+++ tsundiary/views/api.py
@@ -4,6 +4,6 @@ from tsundiary.views import *
 def api_my_current_entry():
     p = g.user.posts.filter(Post.posted_date == g.date).first()
     if p:
-        return json.dumps({ 'timestamp': unix_timestamp(p.update_time), 'content': p.content })
+        return json.dumps({ 'timestamp': unix_timestamp(p.update_time), 'content': p.content, "datestamp": datestamp(p.posted_date) })
     else:
         return json.dumps({ 'timestamp': 'null', 'content': '' })
