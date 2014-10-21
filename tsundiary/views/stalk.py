e567b6080a98e068c1cd20c65cc241738e60ca6f
Jim Zhang <hyriodula@gmail.com>
2014-10-21 17:28:09 -0400

    save stalks

--- tsundiary/views/stalk.py
+++ tsundiary/views/stalk.py
@@ -1,10 +1,15 @@
 from tsundiary.views import *
 
-# Stalk a list of users.
+# Stalk a list of authors.
 @app.route('/stalk/<authors>')
 def stalk_list_given(authors):
     authorlist = authors.split(',')
     posts = []
+
+    # Save stalks
+    if g.user:
+        g.user.stalks = authors
+        db.session.commit()
 
     for authorname in authorlist:
         author = User.query.filter_by(sid = uidify(authorname)).first()
@@ -22,7 +27,34 @@ def stalk_list_given(authors):
 
     return render_template('stalk.html', posts=posts)
 
+# Add a user to a user's stalk list.
+@app.route('/stalkadd', methods=['POST'])
+def stalk_list_add():
+    add_author = request.form.get('victim')
+    if not g.user:
+        return page_not_found()
+    g.user.stalks += "," + add_author
+    db.session.commit()
+    return redirect('/stalk')
+
+# Remove a user from a user's stalk list.
+@app.route('/stalkdel', methods=['POST'])
+def stalk_list_del():
+    del_author = request.form.get('victim')
+    if not g.user:
+        return page_not_found()
+    authorlist = g.user.stalks.split(',')
+    authorlist = [a for a in authorlist if uidify(a) != uidify(del_author)]
+    g.user.stalks = ','.join(authorlist)
+    db.session.commit()
+    return redirect('/stalk')
+
 # Stalk users.
 @app.route('/stalk')
 def stalk_list():
-    return render_template('stalk.html')
+    if g.user and g.user.stalks:
+        return stalk_list_given(g.user.stalks)
+    elif g.user:
+        return render_template('stalk.html')
+    else:
+        return page_not_found()
