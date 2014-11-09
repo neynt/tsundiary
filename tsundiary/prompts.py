9988820729bd96b0afaf1e228e37339003b6b3ce
Jim Zhang <hyriodula@gmail.com>
2014-11-09 13:20:58 -0500

    remove unused prompts, add entry count at top

--- tsundiary/prompts.py
+++ tsundiary/prompts.py
@@ -1,9 +1,11 @@
-""" Contains all the randomly generated little messages. """
 # encoding=utf-8
+# All the cute little randomly-generated messages.
 PROMPTS = [
+
+    # TSUN
+
     "... well, %s?",
     "... hello, %s?",
-    ## Normal "'sup" prompts
     "... did you manage to accomplish anything today, %s?",
     "... so, %s, what did you manage to accomplish today, if anything?",
     "Hey %s - found any new ways to make a fool of yourself?",
@@ -15,30 +17,19 @@ PROMPTS = [
     "It's your privilege that I'm wasting my time listening to you, %s...",
     "Don't get me wrong, %s, it's not like I'm worried about you.",
     "If you think I'm gonna miss you, think again, %s.",
-    "I-it's not like I'm listening to you because I like you or anything, %s...",
-    ## More specific prompts
     "How did it go, %s? ... not that I'm expecting much!",
     "I'll forgive you, but just this time, got it, %s?",
     "... who do you think you are, %s?",
     "Why would you do that, %s? You are so foolish...",
-    ## Calling the end-user an idiot
-    ##"バカバカバカ！",
-    #"AAAAAH, %s, you idiot-idiot-idiot!",
-    #"%s, you're such a moron.",
-    #"%s, you're such an idiot.",
-    #"%s, you're such a fool.",
-    #"%s wa baka desu.",
-    #"%s no baka baka baka!",
     "Can you be any more clueless, %s?",
     "It's cute how you have no idea what's going on around you, %s.",
-    ## Backhanded compliments
-    #"I guess you're smarter than you look, %s...",
-    #"You look so good today, %s! I didn't recognize you."
-    # make me happy
+
+    # DERE
+
+    "I-it's not like I'm listening to you because I like you or anything, %s...",
     "You look good today, %s!",
     "You may not know this, but you have many admirers, %s!",
     "You have a really nice smile, %s! Please smile more...",
-    "Hi, %s!",
     "Tell me about your day, %s!",
     "What made you happy today, %s?",
     "What made you laugh today, %s?",
@@ -46,14 +37,10 @@ PROMPTS = [
     "How are you, %s?",
     "How did your day go, %s?",
     "Feel free to let me know if something is bothering you, %s...",
-    #"I admire your optimism, %s!",
     "I love listening to you, %s!",
-    #"I'm so lucky to have you, %s!",
+    "I'm so lucky to have you, %s!",
     "I'm so happy I'm your friend, %s!",
-    #"I hope you can find happiness, %s!",
-    #"I really like your personality, %s!",
     "Keep going, %s!",
     "%s, m-may I have a hug?",
-    #"%s, w-would you like a hug?",
-    #u"%sのことが大好きです！(*/////∇/////*)",
+    "%s, w-would you like a hug?",
 ]
