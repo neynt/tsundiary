a5bd619fcb4af2a3e00cf08ecb6ff8a794236d77
Jim Zhang <hyriodula@gmail.com>
2015-02-07 19:34:50 -0500

    remove userlist

--- tsundiary/views/userlist.py
+++ tsundiary/views/userlist.py
@@ -1,7 +1,7 @@
 from tsundiary.views import *
 
 # List of users.
-@app.route('/userlist')
+#@app.route('/userlist')
 def userlist():
     return page_not_found()
     all_users = (User.query.order_by(User.num_entries.desc())
@@ -11,7 +11,7 @@ def userlist():
     return render_template('userlist.html', all_users=all_users)
 
 # List of users (sorted by latest new post)
-@app.route('/userlist/latest')
+#@app.route('/userlist/latest')
 def userlist_latest():
     return page_not_found()
     all_users = (User.query.order_by(User.latest_post_date.desc())
@@ -23,7 +23,7 @@ def userlist_latest():
     return render_template('userlist.html', all_users=all_users)
 
 # List of users sorted by latest post time (including throwaways).
-@app.route('/secret_userlist')
+#@app.route('/secret_userlist')
 def userlist_all():
     all_users = (User.query.order_by(User.latest_post_date.desc())
             .order_by(User.num_entries.desc())
